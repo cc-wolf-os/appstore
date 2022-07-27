036c8abff4fdd309e137381efc887f8edf9de76a
badgeminer <64038298+badgeminer@users.noreply.github.com>
2022-07-27 13:44:18 -0600

    Create check.py

---
+++ check.py
@@ -0,0 +1,6 @@
+import logging
+logging.error(
+                "Invalid CNAME '%s' for '%s'. This should be a domain name, not a URL",
+                cname,
+                domain,
+            )
