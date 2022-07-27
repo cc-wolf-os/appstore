0c20ab488bb1fb2d09844f9b2a7057714ae240cc
badgeminer <64038298+badgeminer@users.noreply.github.com>
2022-07-27 14:53:53 -0600

    Update check.py

--- check.py
+++ check.py
@@ -1,5 +1,6 @@
 import logging,sys
 import programs
+from urllib.parse import urlparse
 valid = 0
 programslist = list(programs.programs.keys())
 sorted_programs_list = sorted(programslist)
@@ -8,6 +9,17 @@ sorted_programs_list = sorted(programslist)
 if programslist != sorted_programs_list:
   valid = 1
   logging.error(
-                "Invalid",
+                "Invalid: programs not sorted",
             )
+for i in programslist:
+  try:
+    url = urlparse(programs.programs[i]["docs"])
+    if not (url.scheme == "http" or url.scheme == "https"):
+      valid = 1
+      logging.error(
+                "Invalid: urls",
+            )
+  except:
+    pass
+ 
 sys.exit(valid)
