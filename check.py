c335a3f4513d6e1752f48016e346dbc7854b015e
badgeminer <64038298+badgeminer@users.noreply.github.com>
2022-07-27 14:03:48 -0600

    Update check.py

--- check.py
+++ check.py
@@ -1,5 +1,12 @@
 import logging
-logging.error(
+valid = 1
+domain_list = list(domains.keys())
+sorted_domain_list = sorted(domain_list)
+
+# Ensure the list is sorted.
+if domain_list != sorted_domain_list:
+  valid = 0
+  logging.error(
                 "Invalid",
             )
-sys.exit(0)
+sys.exit(valid)
