bd649e0d5aa80bcd489f9ba5a88cdc1a9c6af895
badgeminer <64038298+badgeminer@users.noreply.github.com>
2022-07-29 08:42:24 -0600

    Update programs.py

--- programs.py
+++ programs.py
@@ -1,6 +1,6 @@
 programs = {
   "test":{
-    "docs":"https://wolf-os.madefor.cc",
+    "docs":"https://wolf-os.madefor.cc/devs",
     "download":"https://raw.githubusercontent.com/cc-wolf-os/apps/main/test.code.txt"
   }
 }
